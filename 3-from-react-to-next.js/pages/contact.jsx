19c70a9ad0729b49e886b1e676ef72bbd830c054
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-06-10 23:23:23 +0200

    Next.js first app

---
+++ 3-from-react-to-next.js/pages/contact.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+ function Title({ content }) {
+  return <h1>{content ? content : "Hello world! 👋"}</h1>;
+}
+
+function LikesButtom() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    console.log(`likes: ${likes}`);
+    setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Likes({likes})</button>;
+}
+
+export default function List() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  return (
+    <div>
+      <Title content="Contact - List of women programmers" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <LikesButtom />
+    </div>
+  );
+}
